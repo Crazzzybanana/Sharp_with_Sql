1f18976fbfd89b5b7a43e70396b4e3b9a2c4c3ae
Dima <crazybanana>
2018-05-18 11:45:27 +0300

    new project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sharp_with_Sql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
